c8ce99220fd71a616964b8bacdc308aca5f91ff3
Samson Kapeyi <skapeyi@bitbucket.org>
2017-02-14 12:41:50 +0300

    Bug fix

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,6 +6,7 @@ use App\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use App\Discussion;
 
 class CommentController extends Controller
 {
